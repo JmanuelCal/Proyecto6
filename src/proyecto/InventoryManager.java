b0bc074a00b3ae2caa4dc4f07ec3567549ae566d
donovan daniel romero machado <donovan.daniel.romero@gmail.com>
2026-02-27 19:00:47 +0100

    Extraer Metodos

--- src/proyecto/InventoryManager.java
+++ src/proyecto/InventoryManager.java
@@ -6,10 +6,19 @@ public class InventoryManager {
 	private static final double IVA = 0.21;
 
 	public void actualizarStock(String producto, int cantidad, double precio) {
-		if (cantidad > 0 && precio > 0 && precio < PRECIO_MAXIMO && producto.length() > LONGITUD_MINIMA_NOMBRE) {
+		if (esEntradaValida(producto, cantidad, precio)) {
 			System.out.println("Stock actualizado: " + producto);
-			double valorTotal = cantidad * precio + cantidad * precio * IVA;
+			double valorTotal = calcularValorTotalConIva(cantidad, precio);
 			System.out.println("Valor total con IVA: " + valorTotal);
 		}
+	}
+
+	private double calcularValorTotalConIva(int cantidad, double precio) {
+		double valorTotal = cantidad * precio + cantidad * precio * IVA;
+		return valorTotal;
+	}
+
+	private boolean esEntradaValida(String producto, int cantidad, double precio) {
+		return cantidad > 0 && precio > 0 && precio < PRECIO_MAXIMO && producto.length() > LONGITUD_MINIMA_NOMBRE;
 	}
 }
