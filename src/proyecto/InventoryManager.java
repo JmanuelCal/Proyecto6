bb5dbaaf1f168f8b19ff8830c17c65ffef2d6daf
José Manuel Calderón Palacios <jmanuel.canito@gmail.com>
2026-03-04 18:20:39 +0100

    Cambios métodos

--- src/proyecto/InventoryManager.java
+++ src/proyecto/InventoryManager.java
@@ -1,11 +1,24 @@
 package proyecto;
 
 public class InventoryManager {
+	private static final int LONGITUD_MINIMA_NOMBRE = 3;
+	private static final int PRECIO_MAXIMO = 10000;
+	private static final double IVA = 0.21;
+
 	public void actualizarStock(String producto, int cantidad, double precio) {
-		if (cantidad > 0 && precio > 0 && precio < 10000 && producto.length() > 3) {
+		if (esValido(producto, cantidad, precio)) {
 			System.out.println("Stock actualizado: " + producto);
-			double valorTotal = cantidad * precio + cantidad * precio * 0.21;
+			double valorTotal = calcularValorConIva(cantidad, precio);
 			System.out.println("Valor total con IVA: " + valorTotal);
 		}
+	}
+
+	private double calcularValorConIva(int cantidad, double precio) {
+		double neto = cantidad * precio;
+		return neto + (neto * IVA);
+	}
+
+	private boolean esValido(String producto, int cantidad, double precio) {
+		return cantidad > 0 && precio > 0 && precio < PRECIO_MAXIMO && producto.length() > LONGITUD_MINIMA_NOMBRE;
 	}
 }
